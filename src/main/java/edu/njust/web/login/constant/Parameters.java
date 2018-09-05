1b076c3cb8cc29c0aa11f5934e441a87dacbdf5e
Jane-He <2243369153@qq.com>
2018-09-05 16:36:10 +0800

    111

--- src/main/java/edu/njust/web/login/constant/Parameters.java
+++ src/main/java/edu/njust/web/login/constant/Parameters.java
@@ -1,5 +1,8 @@
 package edu.njust.web.login.constant;
 
+/**
+ * 这个类是用来记录常量，配置信息
+ */
 public class Parameters {
     public static final int TOKEN_EXPIRE_TIME = 5;  //单位是min
     public static final int CAPTCHA_EXPIRE_TIME = 1;  //单位是min
